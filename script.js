5423e16a593ff4ae4a4c9ae3c81372e8aac1dfc1
Stan <stanedgar@gmail.com>
2025-04-22 21:13:32 -0600

    Data displayed on HTML, Can Search Cities & Changing BG

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
 const para = document.getElementById('para');
+const search = document.querySelector("#search");
+const body = document.body;
 
-        const show = () => {
-        fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Belize%20City/today?unitGroup=metric&key=W2T8ERRJ9ANBDRECKCY3AJUWS&contentType=json' ,{mode: 'cors'})
-        .then(function(response) {
-            return response.json()
-        })
-        .then(function(response) {
-            console.log(response)
 
-            const temp = response.currentConditions.temp;
-            const humidity = response.currentConditions.humidity;
-            const wind = response.currentConditions.windspeed;
+const show = async () => {
 
-            console.log(`Temperature: ${temp}, Humidity: ${humidity}, wind Speed: ${wind}`)
-            para.innerHTML = `Temperature: ${temp}<br> Humidity: ${humidity}<br> wind Speed: ${wind}`;
-        })
-        .catch((response) => {
-            console.error("Something failed and that shouldn't happen.....")
-        })
-    }
+    try {
+const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${search.value}%20City/today?unitGroup=metric&key=W2T8ERRJ9ANBDRECKCY3AJUWS&contentType=json` ,{mode: 'cors'})
+const json = await response.json();
+
+
+    console.log(json)
+
+    const temp = json.currentConditions.temp;
+    const humidity = json.currentConditions.humidity;
+    const wind = json.currentConditions.windspeed;
+
+    if (temp > 32) {
+        body.className = "warm";
+    } else if (temp < 20) {
+        body.className = "cold";
+    } 
+
+    console.log(`Temperature: ${temp}, Humidity: ${humidity}, wind Speed: ${wind}`)
+    para.innerHTML = `Temperature (Farenheit): ${temp}<br> Temperature (Celsius): ${(temp - 32) * 5 / 9}<br> Humidity: ${humidity}%<br> wind Speed: ${wind} mph`;
+} catch(error) {
+    console.error("Something failed and that shouldn't happen.....")}
+};
